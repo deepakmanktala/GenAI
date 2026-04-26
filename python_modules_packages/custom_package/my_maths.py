57152fe710cfc4daaeb2b58b8380023cb07639a9
Deepak Manktala <deepak.manktala@gmail.com>
2026-04-27 04:09:09 +0530

    Added new python programs for Machine Learning and AI basics

---
+++ python_modules_packages/custom_package/my_maths.py
@@ -0,0 +1,5 @@
+def addition(a,b):
+    return a+b
+
+subtraction = lambda a,b : a - b
+# print(subtraction(5,10))
---
+++ python_modules_packages/test.py
@@ -0,0 +1,3 @@
+from custom_package import my_maths
+print(my_maths.addition(7,10))
+print(my_maths.subtraction(90,60))
