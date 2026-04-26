818ed6f7e7f3ef0c69b73863bd2331a5f7fd64c3
Deepak Manktala <deepak.manktala@gmail.com>
2026-04-27 04:17:49 +0530

    custom packages and modules

--- python_modules_packages/test.py
+++ python_modules_packages/test.py
@@ -1,3 +1,11 @@
 from custom_package import my_maths
+
+from custom_package.subpackage import multi
+
+from custom_package.subpackage.multi import multiply, divide
+
 print(my_maths.addition(7,10))
 print(my_maths.subtraction(90,60))
+
+print(multi.divide(10,5))
+print(multi.multiply(90,10))
